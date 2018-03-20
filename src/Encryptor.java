78de7f1d06174924aab7136c6c99765daf04344e
Justin Haughton <haughtj1@stem101-mac3.local>
2018-03-20 18:32:51 -0400

    Major Changes

--- src/Encryptor.java
+++ src/Encryptor.java
@@ -1,44 +1,42 @@
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.Random;
-import java.util.concurrent.ThreadLocalRandom;
 
 public class Encryptor {
 	public static void main(String args[]) {
+		int keySize = 10;
+		
 		String original = "Hello World! This is some sensitive information that I don't want you to see.";
 		String encrypted = "";
 		
 		System.out.println("Original Message: " + original);
 		
 		System.out.println("Attempting to generate key");
-		long p1 = 0;
-		boolean firstPrimeFound = false;
-		long p2 = 0;
-		boolean secondPrimeFound = false;
 		
-		Random rand = new Random(System.currentTimeMillis());
-		while(!firstPrimeFound) {
-			//we want our primes to be random, large and similar size
-			p1 = ThreadLocalRandom.current().nextLong( (long)(Math.pow(2, 60)), (long)(Math.pow(2, 61))); 
-			System.out.println("Checking if " + p1 + " is a prime number");
-			
-			//now we try fermat little's theorem to see if we have a prime
-			boolean fermats1 = false;
-			boolean fermats2 = false;
-			while(!fermats1 && !fermats2) {
-				long a = ThreadLocalRandom.current().nextLong(2, p1); //random a not divisible by p1
-				
-				long temp = (long)Math.pow(a, (p1 - 1)); //we want to see if this is congruent with 1 modulo p
-				temp--;
-				
-				if(temp % p1 == 0) {
-					
-				}
-			}
-			
-			
-			
+		BigInteger p1 = BigInteger.probablePrime(keySize, new Random());
+		BigInteger p2 = BigInteger.probablePrime(keySize, new Random());
+		
+		
+		BigInteger n = p1.multiply(p2); //modulus for the public key and private key
+		
+		BigInteger totient = p1.subtract(BigInteger.ONE);
+		totient = totient.multiply(p2.subtract(BigInteger.ONE));
+		
+		//gives us the size to help create public key exponent
+		BigInteger size = BigInteger.TWO;
+		int numBits;
+		for(numBits = 0; size.pow(numBits).compareTo(totient) == -1; numBits++) {
 		}
 		
+		BigInteger e = BigInteger.probablePrime(numBits - 1, new Random());
 		
-		
+		BigInteger d = null;
+				
+		System.out.println("generated prime number " + p1.toString());
+		System.out.println("generated prime number " + p2.toString());
+		System.out.println("totient " + totient.toString() + " is " + numBits + " bits long");
+		System.out.println("public key exponent is " + e.toString());
+		System.out.println("private key exponent is " + e.toString());
 	}
+	
 }
